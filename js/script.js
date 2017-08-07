bf46f83edc2470e2bc2ea6ae7dd6afdb8a24e893
chris flan <flanthedev@gmail.com>
2017-08-07 16:04:02 +0800

    progress on rendering html

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
+const quizElement = document.getElementById('quiz');
 const questionElement = document.getElementById('question');
 const progressElement = document.getElementById('progress');
 const choiceElements = document.querySelectorAll('.choice');
 const guessElements = document.querySelectorAll('.guess');
 const guesses = [];
+let currentQuestion = 0;
+let wrongGuesses = 0;
+let rightGuesses = 0;
 
+// QUIZ CONSTRUCTOR
 function Quiz() {
   this.questions = [];
 }
-const quiz1 = new Quiz();
+Quiz.prototype.add = function(question) {
+  this.questions.push(question);
+}
+Quiz.prototype.render = function(currentQuestion) {
+  if (this.questions[currentQuestion]) {
+    this.questions[currentQuestion].toHTML();
+  } else {
+    progressElement.innerHTML += ' All Done! Great Job!';
+    quizElement.innerHTML = '';
 
+  }
+}
+
+// QUESTiONS CONSTRUCTOR
 function Question(question, answer, choices, guess) {
   this.question = question;
   this.answer = answer;
@@ -27,13 +44,19 @@ Question.prototype.toHTML = function() {
 }
 Question.prototype.check = function() {
   if (this.answer === guesses.indexOf( this.answer ) ) {
-    console.log('correct');
+    rightGuesses += 1;
   } else {
-    console.log('incorrect');
+    wrongGuesses += 1;
   }
 }
-const question1 = new Question("Who is the best quarterback ever?", 0, ["Tom Brady", "Joe Montana", "John Elway", ]);
-question1.toHTML();
+
+// INITIALIZE QUIZ AND QUESTIONS
+const quiz1 = new Quiz();
+const question1 = new Question("Who is the best quarterback ever?", 0, ["Tom Brady", "Joe Montana", "John Elway"]);
+const question2 = new Question("What color is the sky?", 1, ["red", "blue", "green"]);
+quiz1.add(question1);
+quiz1.add(question2);
+quiz1.render(currentQuestion);
 
 function buttonListener() {
   for (i = 0; i < guessElements.length; i++) {
@@ -42,8 +65,12 @@ function buttonListener() {
       console.log('test' + num);
       guesses.push( num );
       question1.check();
-    });
 
+      progressElement.innerHTML = 'You have answered ' + rightGuesses +
+                      ' correctly and ' + wrongGuesses + ' incorrectly.';
+      currentQuestion += 1;
+      quiz1.render(currentQuestion);
+    });
   }
 }
 buttonListener();
